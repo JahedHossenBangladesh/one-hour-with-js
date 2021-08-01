fb014753d052fa2d6e46cde951d60a2ae7905cdf
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 17:46:10 +0600

    switch case

---
+++ switch.js
@@ -0,0 +1,12 @@
+let guest;
+
+switch (guest) {
+    case "admin":
+        console.log("Hello Admin");
+        break;
+    case "user":
+        console.log("Hello User");
+        break;
+        default:
+            console.log("Hello Guest");
+}
