a7e463dc60dc34ae7e55665afdfa576693bbe32b
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 17:55:22 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,8 @@
+for(let i = 0; i < 5; i++){
+    // console.log('hellow',i);
+    // if( i % 2 !== 0) console.log('odd',i);
+//     i%2 === 0 && console.log('even',i);
+i%2 === 0 ? console.log('event',i) : console.log('odd',i);
+
+
+}
