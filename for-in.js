489fe729999225711e0e6d6d607ff54b7e676a16
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 18:13:13 +0600

    this is for in

---
+++ for-in.js
@@ -0,0 +1,15 @@
+let person = {
+    name : "jahed",
+    age : 30
+}
+
+for(let key in person) {
+    console.log(key,person[key]);
+}
+
+let color = ["red","white","green" ];
+for (
+    let index in color
+){
+console.log(index,color[index]);
+}
