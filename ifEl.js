ab1f7a80ffad9116b9d0c956af4c3f5e480d62d5
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 17:22:32 +0600

    if else

---
+++ ifEl.js
@@ -0,0 +1,21 @@
+// if ( condition) {
+//     statement
+// }
+// else if(anotherCondition){
+//     statement
+// }
+// else if(anotherCondition){
+//     statement
+// }
+// else{ statement
+// }
+
+var hour = 10;
+
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good AfterNoon");
+} else {
+  console.log("Good evening");
+}
