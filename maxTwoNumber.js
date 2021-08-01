03a87d34d54d28363d592eeae95717f7dc0cd4ec
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 19:42:08 +0600

    find the maximum number

---
+++ maxTwoNumber.js
@@ -0,0 +1,12 @@
+function maxTwoNumber(num1, num2) {
+  var max = num1 > num2 ? num1 : num2;
+  return max;
+}
+let checkTwoNumber = maxTwoNumber(1,2);
+console.log(checkTwoNumber);
+
+function max(num1,num2){
+    return num1 > num2 ? num1 :num2;
+}
+let check = max(1,3);
+console.log(check);
