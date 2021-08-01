2f5271586834bb7b57774fa5a57e3f107562d4cd
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-01 18:03:12 +0600

    do while loop is one time run if the condition is true or false

---
+++ while.js
@@ -0,0 +1,13 @@
+// let i = 0;
+
+// while (i <= 5){
+//     i%2 === 0 ? console.log("even",i) :console.log("odd",i);
+//     i++;
+// }
+
+let i = 8;
+
+do {
+    i%2 === 0 ? console.log("even",i) :console.log("odd",i);
+    i++;
+} while (i <= 5);
